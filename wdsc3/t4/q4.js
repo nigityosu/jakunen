6d4c1c0571a10c76d9dff5af8ef541e61103aad8
nigityoth <kawai891204@gmail.com>
2026-04-21 17:30:44 +0900

    4/21

---
+++ wdsc3/t4/q4.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.menu')
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('is-active')
+    btn.classList.toggle('is-active')
+})
